31d5b1e43232c08d7499ca2d895e09ddc8fa262b
Greta Mitovska <gretamitovska@hotmail.com>
2020-06-12 16:13:01 +0200

    Project added

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*
+
+class SILab2Test {
+
+    @Test
+    void function() {
+        assertEquals(function(null););
+    }
+
+    @Test
+    void everyStatement(){
+
+        assertEquals(function(0,#,0););
+
+    }
+}
